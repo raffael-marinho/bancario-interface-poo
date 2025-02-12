6c871c82692d3bb0cbc08e4eb5b96dccca7811ed
raffael-marinho <silvamarinho10@gmail.com>
2025-02-12 15:15:14 -0300

    feat: add methods IConta in ContaCorrent and ContaPoupanca

--- bancario_interface_poo/src/model/ContaCorrente.java
+++ bancario_interface_poo/src/model/ContaCorrente.java
@@ -1,5 +1,31 @@
 package model;
 
-public class ContaCorrente {
+import java.math.BigDecimal;
+
+public class ContaCorrente implements IConta {
+
+	@Override
+	public boolean realizarSaque(BigDecimal quantia) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BigDecimal consultarSaldo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean realizarDeposito(BigDecimal quantia) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void realizarTransferencia(IConta destino, BigDecimal quantia) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
--- bancario_interface_poo/src/model/ContaPoupanca.java
+++ bancario_interface_poo/src/model/ContaPoupanca.java
@@ -1,5 +1,31 @@
 package model;
 
-public class ContaPoupanca {
+import java.math.BigDecimal;
+
+public class ContaPoupanca implements IConta {
+
+	@Override
+	public boolean realizarSaque(BigDecimal quantia) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public BigDecimal consultarSaldo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean realizarDeposito(BigDecimal quantia) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void realizarTransferencia(IConta destino, BigDecimal quantia) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
