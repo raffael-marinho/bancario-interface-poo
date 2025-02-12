bb6ada1ce4d01a24b77389cb501095cbdb505d0c
raffael-marinho <silvamarinho10@gmail.com>
2025-02-12 16:14:27 -0300

    feat: add methods constructor, getters and setters, hashCode and equals, toString in Cliente

--- bancario_interface_poo/src/model/Cliente.java
+++ bancario_interface_poo/src/model/Cliente.java
@@ -1,5 +1,70 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Cliente {
+
+	private String cpf;
+	private String nome;
+
+	private ArrayList<IConta> contas = new ArrayList<>();
+
+	public Cliente(String cpf, String nome) {
+		super();
+		this.cpf = cpf;
+		this.nome = nome;
+	}
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public ArrayList<IConta> getContas() {
+		return contas;
+	}
+
+	public void setContas(ArrayList<IConta> contas) {
+		this.contas = contas;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(cpf);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(cpf, other.cpf);
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [cpf=" + cpf + ", nome=" + nome + ", contas=" + contas + "]";
+	}
 
 }
